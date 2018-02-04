8abd8815555f43d8f2b5443896373d2f3ee8d27c
kylesallette <kylesallette@gmail.com>
2018-02-04 16:50:28 -0700

    moving forward

---
+++ spec/features/user_sees_all_articles_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+  describe "user sees all articles" do
+    describe "they visit /articles" do
+      it "displays all articles" do
+        article_1 = Article.create!(title: "Title 1", body: "body")
+        article_2 = Article.create!(title: "Title 2", body: "Body 2")
+
+        visit '/articles'
+
+        expect(page).to have_link(article_1.title)
+        expect(page).to have_content("Title 1")
+        expect(page).to have_link(article_2.title)
+        expect(page).to have_link("Create a New Article")
+      end
+    end
+  end 
+
+    describe "user see one article" do
+      describe 'they link from the article index' do
+        it "displays information for one article" do
+          article = Article.create!(title: "New Title", body: "New Body")
+
+          visit articles_path
+
+          click_link article.title
+
+          expect(page).to have_content(article.title)
+          expect(page).to have_content(article.body)
+
+        end
+      end
+    end
